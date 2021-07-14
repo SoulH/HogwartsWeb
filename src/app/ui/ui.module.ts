e765f4f0b4baa857741cb87e7f0439dca228e6ee
Saul Hernandez <saul7.hernandez@gmail.com>
2021-07-14 10:29:51 -0400

    routing to pages

---
+++ src/app/ui/ui.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DefaultLayoutComponent } from './layouts/default-layout/default-layout.component';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultLayoutComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class UiModule { }
